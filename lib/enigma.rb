127b3c0d212d17b55703fcab9672c0ca2b092e26
Paker Thomson <pdthomson1239@gmail.com>
2022-06-10 19:30:26 -0400

    write first draft of encrypt method

---
+++ lib/enigma.rb
@@ -0,0 +1,16 @@
+class Enigma
+
+  def encrypt(message, key, date)
+    enigma_hash = {}
+    enigma_hash[:encryption] = "keder ohulw"
+    enigma_hash[:key] = key
+    enigma_hash[:date] = date
+    enigma_hash
+  end
+
+
+
+
+
+
+end
