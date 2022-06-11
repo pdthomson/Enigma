db8463726df51c42bf2279ac9d6a03ee0f06300e
Paker Thomson <pdthomson1239@gmail.com>
2022-06-11 14:39:03 -0400

    add to method to correct edge case issues

--- modules/shiftable.rb
+++ modules/shiftable.rb
@@ -13,11 +13,19 @@ module Shiftable
   end
 
   def shift_array(offset, key)
-    offset.each_char.with_index.map {|char, index| char.to_i + key.slice(index..index+1).to_i}
+    offset.each_char.with_index.map {|offset_char, index_string| offset_char.to_i + key.slice(index_string..index_string+1).to_i}
   end
 
-
-
-
-
+  def encode(message, shift)
+    encrypted_string = ''
+    message.each_char do |string|
+      if characters.include?(string) == nil?
+      else
+      shift_letter = characters.index(string) + shift[0]
+      encrypted_string.concat(characters.rotate(shift_letter)[0])
+      shift.rotate!
+      end
+    end
+    encrypted_string
+  end
 end
