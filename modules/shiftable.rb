7fa13233e451c596d33ebd038412201564a2ba9f
Paker Thomson <pdthomson1239@gmail.com>
2022-06-11 13:05:28 -0400

    Add the method to make the shift_array for the encryption

--- modules/shiftable.rb
+++ modules/shiftable.rb
@@ -7,7 +7,14 @@ module Shiftable
     ("a".."z").to_a << " "
   end
 
+  def create_offset(date)
+    offset = (date.to_i) **2
+    offset.to_s.slice(-4..-1)
+  end
 
+  def shift_array(offset, key)
+    offset.each_char.with_index.map {|char, index| char.to_i + key.slice(index..index+1).to_i}
+  end
 
 
 
